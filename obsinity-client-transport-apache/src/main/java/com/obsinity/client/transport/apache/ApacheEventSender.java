b29ba1b8580fbbad208b712a7012b96459fd3da7
John Allen <john.allen@mastercard.com>
2025-08-21 13:16:47 +0100

    Dummy jars building

--- obsinity-client-transport-apache/src/main/java/com/obsinity/client/transport/apache/ApacheEventSender.java
+++ obsinity-client-transport-apache/src/main/java/com/obsinity/client/transport/apache/ApacheEventSender.java
@@ -2,7 +2,7 @@ package com.obsinity.client.transport.apache;
 
 import com.obsinity.client.transport.EventSender;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.ByteArrayEntity;
 import org.apache.hc.core5.http.ContentType;
