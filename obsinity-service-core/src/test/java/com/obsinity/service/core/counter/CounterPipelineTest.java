da813a1599a61845c03cb865920df70556a21ca9
John J. Allen <john.joe.allen@gmail.com>
2025-11-15 19:00:12 +0000

    test(counters): stabilize pipeline assertions

--- obsinity-service-core/src/test/java/com/obsinity/service/core/counter/CounterPipelineTest.java
+++ obsinity-service-core/src/test/java/com/obsinity/service/core/counter/CounterPipelineTest.java
@@ -1,7 +1,6 @@
 package com.obsinity.service.core.counter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
